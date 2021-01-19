62b2dc45062dcd4b8777c92ca291e02db007385d
nataliacherman08 <cherman.natalia08@gmail.com>
2021-01-19 10:46:22 +0100

    changes

--- exercice0104.js
+++ exercice0104.js
@@ -4,5 +4,4 @@ let name = readlineSync.question('What is your name ?');
 let firstName = readlineSync.question('What is your first name ?');
 let city = readlineSync.question('Where do you live ?');
 
-
-console.log("Your name is "+ firstName + " " + name +" and you live in "+ city +".");
+console.log("Your name is " + firstName + " " + name +" and you live in "+ city +".");
---
+++ exercice0107.js
@@ -0,0 +1,6 @@
+const readlineSync = require("readline-sync");
+
+let x = readlineSync.question('Can you give me your shoe size?');
+let y = readlineSync.question('Can you give me your birth year?');
+
+console.log(x*2);
