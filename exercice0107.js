3e3e90d17aef31817edf9cab67896dd134e09755
nataliacherman08 <cherman.natalia08@gmail.com>
2021-01-19 11:02:32 +0100

    Update exercice0107.js

--- exercice0107.js
+++ exercice0107.js
@@ -3,4 +3,10 @@ const readlineSync = require("readline-sync");
 let x = readlineSync.question('Can you give me your shoe size?');
 let y = readlineSync.question('Can you give me your birth year?');
 
-console.log(x*2);
+let a = x*2;
+let b = a+5;
+let c = b*50;
+let d = c-y;
+let e = d+1766;
+
+console.log(e);
