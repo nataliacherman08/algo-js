28107a6ac9a94dc0f8fb7365d404b8b94ce860ca
nataliacherman08 <cherman.natalia08@gmail.com>
2021-01-21 20:28:58 +0100

    Update 3.1.js

--- arrays/3.1.js
+++ arrays/3.1.js
@@ -1,4 +1,4 @@
 const arr = [1, 2, 3, 4, 5];
-let total = 0;
-for (e of arr) total += e;
-console.log(`The amount is ${total}`);
+let sum = 0;
+for (e of arr) sum += e;
+console.log(`The amount is ${sum}`);
