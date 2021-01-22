45943340356c06517b3c1574f888c3c83523d906
nataliacherman08 <cherman.natalia08@gmail.com>
2021-01-22 10:55:48 +0100

    Update 4.1

--- functions/4.1.js
+++ functions/4.1.js
@@ -1,5 +1,14 @@
-function calcSurface(a,b) {
-    return a*b;
+const readlineSync = require("readline-sync");
+let x = readlineSync.question("What is the length of your rectangle?");
+let y = readlineSync.question("What is the width of your rectangle?");
+
+function calcSurface (x,y) {
+    return x*y;
 }
 
-console.log(calcSurface(10,5));
+console.log(calcSurface(x,y));
+
+// The surface of a rectangle is calculated by multiplying the width with the length.
+// I assigned a length (by asking it to the user) to x and a width to y.
+// I used that values in the parameters of my function and asked to multiply them in return.
+// Then, finally, I ask with my console.log to display my function.
