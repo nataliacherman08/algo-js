ad78edf2cbe4fafaf3b5aa0202ac8f305cea2431
nataliacherman08 <cherman.natalia08@gmail.com>
2021-01-22 10:15:03 +0100

    Update 3.3bis

--- arrays/3.3bis.js
+++ arrays/3.3bis.js
@@ -1,4 +1,16 @@
-let arr = ['Apple', 'Banana'];
-let shallowCopy = arr.slice();
+//Method 1
+let arr = [1, 2, 3, 4, 5];
+let newArr = [...arr];
+console.log(newArr);
 
-console.log(arr.slice(0,2));
+
+//Method 2
+/*let arr = [1, 2, 3, 4, 5];
+let newArr = arr.slice();
+console.log(arr.slice(0,5));*/
+
+
+//Method 3
+/*let arr = [1, 2, 3, 4, 5];
+let newArr = Array.from(arr);
+console.log(newArr);*/
