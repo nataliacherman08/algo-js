47bd632a4d6e90af97558c601d8e5e3589553780
nataliacherman08 <cherman.natalia08@gmail.com>
2021-01-22 09:37:49 +0100

    Update arrays

--- arrays/3.2.js
+++ arrays/3.2.js
@@ -2,8 +2,8 @@ let arr = [1,2,3,4,5];
 var total = 0;
 var count = 0;
 
-for(var i = 0; i < someArray.length; i++){
-    total += someArray[i];
+for(var i = 0; i < arr.length; i++){
+    total += arr[i];
     count++;
 }
 
