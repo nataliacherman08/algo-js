0d65c765305a119672c3a817ba6a157330148501
nataliacherman08 <cherman.natalia08@gmail.com>
2021-01-22 13:11:56 +0100

    Update 4.3.js

--- functions/4.3.js
+++ functions/4.3.js
@@ -1,20 +1,25 @@
 const readlineSync = require("readline-sync");
-let n = Number(readlineSync.question("How many numbers would you like to generate ?"));
+let n = Number(readlineSync.question("How many numbers between 1 and 10 would you like to generate ?"));
 
 function rand10(min,max) {
     min = Math.ceil(1);
     max = Math.floor(10);
     return Math.floor(Math.random() * (max - min +1)) + min;
-  }
-
-//console.log(rand10());
-
-function multiRand(n) {
-    for (let i=0; i < n; i++) {
-        arr.push([i]);
-    }
-    rand10(n);
-    return arr[i];
 }
 
-console.log(multiRand());
+function multiRand(n) {
+    let arr = [];
+    for (let i=0 ; i<n; i++) {
+        arr.push(rand10());
+    }
+    return arr;
+}
+
+console.log("Here are the generated numbers: "+ multiRand(n));
+
+/**I created an array in which I add the random generated numbers with help of the function rand10().
+
+In this programm, I have to generate the amount of times the user wants to generate those numbers.
+That amount is determined by my question. I attribuate to this amount the value n.
+n will serve me to create a loop inside my function multiRand().
+This loop will determine the amount of time the numbers will be generated.**/
